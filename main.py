66d43c019b9e72879afb9ea3be6bcb4d1d38f81d
WeakGT <jasonhuang921003@gmail.com>
2024-07-06 15:18:32 +0800

    update google search API

--- main.py
+++ main.py
@@ -107,65 +107,75 @@ async def handle_callback(request: Request):
             else:
                 messages = chatgpt
 
-            bot_condition = {
-                "清空": 'A',
-                "摘要": 'B',
-                "地震": 'C',
-                "氣候": 'D',
-                "其他": 'E'
-            }
-
-            model = genai.GenerativeModel('gemini-1.5-pro')
-            response = model.generate_content(
-                f'請判斷 {text} 裡面的文字屬於 {bot_condition} 裡面的哪一項？符合條件請回傳對應的英文文字就好，不要有其他的文字與字元。')
-            print('='*10)
-            text_condition = re.sub(r'[^A-Za-z]', '', response.text)
-            print(text_condition)
-            print('='*10)
-            if text_condition == 'A':
-                fdb.delete(user_chat_path, None)
-                reply_msg = '已清空對話紀錄'
-            elif text_condition == 'B':
-                model = genai.GenerativeModel('gemini-pro')
-                response = model.generate_content(
-                    f'Summary the following message in Traditional Chinese by less 5 list points. \n{messages}')
-                reply_msg = response.text
-            elif text_condition == 'C':
-                print('='*10)
-                print("地震相關訊息")
-                print('='*10)
-                model = genai.GenerativeModel('gemini-pro-vision')
-                OPEN_API_KEY = os.getenv('OPEN_API_KEY')
-                earth_res = requests.get(f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/E-A0015-003?Authorization={OPEN_API_KEY}&downloadType=WEB&format=JSON')
-                url = earth_res.json()["cwaopendata"]["Dataset"]["Resource"]["ProductURL"]
-                reply_msg = check_image_quake(url)+f'\n\n{url}'
-            elif text_condition == 'D':
-                location_text = '台北市'
-                location = check_location_in_message(location_text)
-                print('Location is: ' + location)
-                weather_data = get_weather_data(location)
-                simplified_data = simplify_data(weather_data)
-                current_weather = get_current_weather(simplified_data)
-
-                print('The Data is: ' + str(current_weather))
-
-                now = datetime.now()
-                formatted_time = now.strftime("%Y/%m/%d %H:%M:%S")
-
-                if current_weather is not None:
-                    total_info = f'位置: {location}\n氣候: {current_weather["Wx"]}\n降雨機率: {current_weather["PoP"]}\n體感: {current_weather["CI"]}\n現在時間: {formatted_time}'
-
-                response = model.generate_content(
-                    f'你現在身處在台灣，相關資訊 {total_info}，我朋友說了「{text}」，請問是否有誇張、假裝的嫌疑？ 回答是或否。')
+            URL = f'https://www.googleapis.com/customsearch/v1?cx=339feef75a8d2425c&key=AIzaSyCZP6s7zMt6Srq00v4a6EsZnTgvPGRv004&q={text}'
+            response = requests.get(URL)
+            if response.status_code == 200:
+                print('success')
                 reply_msg = response.text
             else:
-                # model = genai.GenerativeModel('gemini-pro')
-                messages.append({'role': 'user', 'parts': [text]})
-                response = model.generate_content(messages)
-                messages.append({'role': 'model', 'parts': [text]})
-                # 更新firebase中的對話紀錄
-                fdb.put_async(user_chat_path, None, messages)
-                reply_msg = response.text
+                print('fail')
+                reply_msg = 'fail'
+
+            # bot_condition = {
+            #     "清空": 'A',
+            #     "摘要": 'B',
+            #     "地震": 'C',
+            #     "氣候": 'D',
+            #     "其他": 'E'
+            # }
+
+            # model = genai.GenerativeModel('gemini-1.5-pro')
+            # response = model.generate_content(
+            #     f'請判斷 {text} 裡面的文字屬於 {bot_condition} 裡面的哪一項？符合條件請回傳對應的英文文字就好，不要有其他的文字與字元。')
+            # print('='*10)
+            # text_condition = re.sub(r'[^A-Za-z]', '', response.text)
+            # print(text_condition)
+            # print('='*10)
+            # if text_condition == 'A':
+            #     fdb.delete(user_chat_path, None)
+            #     reply_msg = '已清空對話紀錄'
+            # elif text_condition == 'B':
+            #     model = genai.GenerativeModel('gemini-pro')
+            #     response = model.generate_content(
+            #         f'Summary the following message in Traditional Chinese by less 5 list points. \n{messages}')
+            #     reply_msg = response.text
+            # elif text_condition == 'C':
+            #     print('='*10)
+            #     print("地震相關訊息")
+            #     print('='*10)
+            #     model = genai.GenerativeModel('gemini-pro-vision')
+            #     OPEN_API_KEY = os.getenv('OPEN_API_KEY')
+            #     earth_res = requests.get(f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/E-A0015-003?Authorization={OPEN_API_KEY}&downloadType=WEB&format=JSON')
+            #     url = earth_res.json()["cwaopendata"]["Dataset"]["Resource"]["ProductURL"]
+            #     reply_msg = check_image_quake(url)+f'\n\n{url}'
+            # elif text_condition == 'D':
+            #     location_text = '台北市'
+            #     location = check_location_in_message(location_text)
+            #     print('Location is: ' + location)
+            #     weather_data = get_weather_data(location)
+            #     simplified_data = simplify_data(weather_data)
+            #     current_weather = get_current_weather(simplified_data)
+
+            #     print('The Data is: ' + str(current_weather))
+
+            #     now = datetime.now()
+            #     formatted_time = now.strftime("%Y/%m/%d %H:%M:%S")
+
+            #     if current_weather is not None:
+            #         total_info = f'位置: {location}\n氣候: {current_weather["Wx"]}\n降雨機率: {current_weather["PoP"]}\n體感: {current_weather["CI"]}\n現在時間: {formatted_time}'
+
+            #     response = model.generate_content(
+            #         f'你現在身處在台灣，相關資訊 {total_info}，我朋友說了「{text}」，請問是否有誇張、假裝的嫌疑？ 回答是或否。')
+            #     reply_msg = response.text
+            # else:
+
+            #     # model = genai.GenerativeModel('gemini-pro')
+            #     messages.append({'role': 'user', 'parts': [text]})
+            #     response = model.generate_content(messages)
+            #     messages.append({'role': 'model', 'parts': [text]})
+            #     # 更新firebase中的對話紀錄
+            #     fdb.put_async(user_chat_path, None, messages)
+            #     reply_msg = response.text
 
             await line_bot_api.reply_message(
                 ReplyMessageRequest(
